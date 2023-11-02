fdde78876e25b530fbd1259cf37ab94cc295b257
Burak Cebecioğlu <53551211+burakcebecioglu@users.noreply.github.com>
2023-11-02 08:41:27 +0100

    Initial Commit

---
+++ Handy Calculator/Handy_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handy_CalculatorApp.swift
+//  Handy Calculator
+//
+//  Created by Burak Cebecioğlu on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Handy_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
